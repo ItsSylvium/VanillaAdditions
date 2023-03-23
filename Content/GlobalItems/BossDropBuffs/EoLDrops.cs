0189b11e36a676687250ed93ed4157c32877e801
Cole <106855960+ItsSylvium@users.noreply.github.com>
2023-03-23 08:29:24 -0500

    Start on EoL

---
+++ Content/GlobalItems/BossDropBuffs/EoLDrops.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using System.Linq;
+
+namespace VanillaAdditions.Content.GlobalItems
+{
+    public class Whips : GlobalItem
+    {
+        public override void SetDefaults(Item item)
+        {
+            if (item.type == 4952) //Nightglow
+            {
+                item.damage = 90;
+            }
+            if (item.type == 4953) //Eventide
+            {
+                item.damage = 100;
+            }
+        }
+    }
+}
